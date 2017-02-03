d8047c8224063415b210638eaf07712c5ef842bd
nemo <vzabudko@gmail.com>
2017-02-03 12:33:45 +0200

    галерея с равной высотой строк

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ $(document).ready(function () {
 $(window).load(function(){
     $('._content').removeClass('hiden');
 
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: 15
-    });
-
+    // $('.grid').masonry({
+    //     itemSelector: '.grid-item',
+    //     columnWidth: 15
+    // });
+    $('._content').gpGallery('.grid-item');
 });
 
 
-$(function(){
-
-    $('img').hover(function () {
-        $(this).next('span').html($(this).attr('alt'));
-    })
-
-});
+// $(function(){
+//
+//     $('img').hover(function () {
+//         $(this).next('span').html($(this).attr('alt'));
+//     })
+//
+// });
